6fb5c3acc266b2e4bd46108eafde3b6373c4f76b
avalberg <valberg@ualberta.ca>
2018-02-12 16:01:54 -0700

    Added Week 5 examples

---
+++ Week 5 - Another Nested Loop Pyramid/PyramidCountingUp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidCountingUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput, printValue = 1;
+
+            Console.WriteLine("Counting Up Inside a Triangle!");
+            Console.WriteLine("------------------------------\n");
+
+            Console.Write("How many rows do you want? ");
+            userInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for(int rows = 1; rows <= userInput; rows++)
+            {
+                for(int columns = 1; columns <= rows; columns++)
+                {
+                    Console.Write("{0} ", printValue++);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Week 5 - Perfect Number/PerfectNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a program that takes a number from the user and checks if it's perfect.
+
+            int userInput, sum = 0;
+
+            Console.WriteLine("Enter a whole number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            Console.Write("The positive divisors of {0} are: ", userInput);
+            for (int i = 1;i <= (userInput / 2); i++)
+            {
+                if (userInput % i == 0)
+                {
+                    sum += i;
+                    Console.Write("{0} ",i);
+                }
+            }
+
+            Console.WriteLine("\nThe sum of these divisors is {0}.", sum);
+            if (sum == userInput)
+            {
+                Console.WriteLine("{0} is perfect!",userInput);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not perfect. But it's still pretty good!",userInput);
+            }
+            Console.WriteLine();
+        }
+    }
+}
