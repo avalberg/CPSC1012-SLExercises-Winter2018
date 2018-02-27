56a63cebadd5ceabcc4e69ac2827df45fe4f3ef8
avalberg <valberg@ualberta.ca>
2018-02-27 12:09:39 -0700

    Added Week 6 Mock InClass 3

---
+++ Week 6 - Mock InClass 3/Question1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // my variables
+            int year;
+            string animal;
+
+            // letting user tell us their YOB
+            Console.Write("What year were you born? ");
+            year = int.Parse(Console.ReadLine());
+
+            // my huge switch!
+            switch ((year - 4) % 12 + 1)
+            {
+                case 1:
+                    animal = "Rat";
+                    break;
+                case 2:
+                    animal = "Ox";
+                    break;
+                case 3:
+                    animal = "Tiger";
+                    break;
+                case 4:
+                    animal = "Rabbit";
+                    break;
+                case 5:
+                    animal = "Dragon";
+                    break;
+                case 6:
+                    animal = "Snake";
+                    break;
+                case 7:
+                    animal = "Horse";
+                    break;
+                case 8:
+                    animal = "Goat";
+                    break;
+                case 9:
+                    animal = "Monkey";
+                    break;
+                case 10:
+                    animal = "Rooster";
+                    break;
+                case 11:
+                    animal = "Dog";
+                    break;
+                case 12:
+                    animal = "Pig";
+                    break;
+                default:
+                    animal = "";
+                    break;
+            }
+
+            // printing the user's year and telling them their animal
+            Console.WriteLine("Since you were born in {0}, your Zodiac animal is a {1}!\n",year,animal);
+        }
+    }
+}
---
+++ Week 6 - Mock InClass 3/Question2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("{0,12}{1:C,12}","test",0);
+
+            // declaring my variables
+            string province, slogan;
+            
+            // asking user for input
+            Console.Write("Enter a province: ");
+            province = Console.ReadLine();
+
+            // switch to determine province!
+            switch (province.ToLower())
+            {
+                case "alberta":
+                    slogan = "Wild Rose Country";
+                    break;
+                case "british columbia":
+                    slogan = "Beautiful British Columbia";
+                    break;
+                case "manitoba":
+                    slogan = "Friendly";
+                    break;
+                case "new brunswick":
+                    slogan = "Picture Province";
+                    break;
+                case "newfoundland and labrador":
+                    slogan = "A World of Difference";
+                    break;
+                case "nova scotia":
+                    slogan = "Canada's Ocean Playground";
+                    break;
+                case "ontario":
+                    slogan = "Yours To Discover";
+                    break;
+                case "prince edward island":
+                    slogan = "Canada's Green Province";
+                    break;
+                case "quebec":
+                    slogan = "Je me souviens";
+                    break;
+                case "saskatchewan":
+                    slogan = "Land of Living Skies";
+                    break;
+                default:
+                    slogan = "";
+                    break;
+            }
+
+            // bonus - a decision to figure out if the province they provided was actually found
+            if (slogan == "")
+            {
+                Console.WriteLine("Sorry, we found no province matching {0}!\n", province);
+            }
+            else
+            {
+                Console.WriteLine("{0}'s provincial slogan is {1}.\n", province, slogan);
+            }
+        }
+    }
+}
